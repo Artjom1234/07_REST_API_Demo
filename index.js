ec4280515d3261f4c882884a4bb3d97db30413be
artikGG <artjomgg@outlook.de>
2022-10-21 10:42:50 +0000

    endPoint

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // import
-constexpress =require('express');
-constapp = express();
-constPORT = 8080;
+const express =require('express');
+const app = express();
+const PORT = 8080;
 
 // Listener
 app.listen(
     PORT,
     ()=> console.log(`Server ready at http://localhost:${PORT}`)
 );
+
+// Endpoint GET
+app.get('/store',(req,res) =>{
+    res.status(200).send({
+        product: "T-Shirt",
+        size: "L",
+        prize: 10.00,
+        hasLogo: false
+    })
+});
